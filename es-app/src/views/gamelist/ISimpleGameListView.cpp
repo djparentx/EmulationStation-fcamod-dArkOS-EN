3f1b8413193e130a5ecb7eaab22e36e07eae57d5
djparentx <276067176+djparentx@users.noreply.github.com>
2026-08-28 00:23:39 -0600

    fix clock, add debug for list views

--- es-app/src/views/gamelist/ISimpleGameListView.cpp
+++ es-app/src/views/gamelist/ISimpleGameListView.cpp
@@ -34,13 +34,6 @@ void ISimpleGameListView::onThemeChanged(const std::shared_ptr<ThemeData>& theme
 {
 	using namespace ThemeFlags;
 	mBackground.applyTheme(theme, getName(), "background", ALL);
-	const ThemeData::ThemeElement* logoElem = theme->getElement(getName(), "logo", "image");
-	LOG(LogInfo) << "[ThemeDebug] view=" << getName() << " logo elemFound=" << (logoElem != nullptr)
-		<< " has(x)=" << (logoElem != nullptr && logoElem->has("x"))
-		<< " has(y)=" << (logoElem != nullptr && logoElem->has("y"))
-		<< " has(pos)=" << (logoElem != nullptr && logoElem->has("pos"))
-		<< " has(origin)=" << (logoElem != nullptr && logoElem->has("origin"));
-
 	mHeaderImage.applyTheme(theme, getName(), "logo", ALL);
 	mHeaderText.applyTheme(theme, getName(), "logoText", ALL);
 
@@ -63,15 +56,6 @@ void ISimpleGameListView::onThemeChanged(const std::shared_ptr<ThemeData>& theme
 	for (auto extra : mThemeExtras)
 	{
 		addChild(extra);
-
-		// This view's own onShow() cascade (which normally activates children like
-		// ClockComponent/BatteryIconComponent/NetworkIconComponent via their mActive flag)
-		// may already have run earlier in this same transition, before these extras existed -
-		// addChild() alone doesn't retroactively fire onShow() on a child added afterward.
-		// Fire it explicitly here so freshly (re)built extras aren't left permanently inactive.
-		// Harmless if the view isn't visible yet - it just means the extra starts computing
-		// its state a little early, and the real onShow() cascade later is a no-op repeat.
-		extra->onShow();
 	}
 
 	if(mHeaderImage.hasImage())
--- es-core/src/components/StackPanelComponent.cpp
+++ es-core/src/components/StackPanelComponent.cpp
@@ -226,7 +226,8 @@ void StackPanelComponent::performLayout()
 
 		if (mHorizontal)
 		{
-			child->setSize(child->getSize().x(), mSize.y());
+			if (child->getSize().y() != mSize.y())
+				child->setSize(child->getSize().x(), mSize.y());
 
 			if (mReverse)
 			{
@@ -243,8 +244,9 @@ void StackPanelComponent::performLayout()
 		}
 		else
 		{
-			child->setSize(mSize.x(), child->getSize().y());
-
+			if (child->getSize().x() != mSize.x())
+				child->setSize(mSize.x(), child->getSize().y());
+			
 			if (mReverse)
 			{
 				float childpos = pos - child->getSize().y() + child->getSize().y() * child->getOrigin().y();
