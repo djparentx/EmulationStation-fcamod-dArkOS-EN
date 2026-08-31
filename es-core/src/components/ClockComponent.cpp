d1be222cf42ad68c164e940272cb972172d13f30
djparentx <276067176+djparentx@users.noreply.github.com>
2026-08-30 23:34:25 -0600

    debug clock

--- es-core/src/components/ClockComponent.cpp
+++ es-core/src/components/ClockComponent.cpp
@@ -1,6 +1,7 @@
 #include "components/ClockComponent.h"
 #include "Settings.h"
 #include "utils/TimeUtil.h"
+#include "Log.h"
 #include <time.h>
 
 ClockComponent::ClockComponent(Window* window) : TextComponent(window), mActive(false)
@@ -13,12 +14,14 @@ void ClockComponent::onShow()
 	TextComponent::onShow();
 	mActive = true;
 	mClockElapsed = 0;
+	LOG(LogInfo) << "[ThemeDebug] ClockComponent::onShow tag=" << getTag();
 }
 
 void ClockComponent::onHide()
 {
 	TextComponent::onHide();
 	mActive = false;
+	LOG(LogInfo) << "[ThemeDebug] ClockComponent::onHide tag=" << getTag();
 }
 
 void ClockComponent::applyTheme(const std::shared_ptr<ThemeData>& theme, const std::string& view, const std::string& element, unsigned int properties)
@@ -31,18 +34,27 @@ void ClockComponent::update(int deltaTime)
 	TextComponent::update(deltaTime);
 
 	if (!mActive)
+	{
+		LOG(LogInfo) << "[ThemeDebug] ClockComponent::update tag=" << getTag() << " SKIPPED mActive=false";
 		return;
+	}
 
 	setVisible(Settings::getInstance()->getBool("DrawClock"));
 
 	if (!isVisible())
+	{
+		LOG(LogInfo) << "[ThemeDebug] ClockComponent::update tag=" << getTag() << " SKIPPED isVisible=false DrawClock=" << Settings::getInstance()->getBool("DrawClock");
 		return;
+	}
 
 	mClockElapsed -= deltaTime;
 	if (mClockElapsed <= 0)
 	{
 		time_t     clockNow = time(0);
 		struct tm  clockTstruct = *localtime(&clockNow);
+
+		LOG(LogInfo) << "[ThemeDebug] ClockComponent::update tag=" << getTag() << " tm_year=" << clockTstruct.tm_year
+			<< " ClockMode12=" << Settings::getInstance()->getBool("ClockMode12");
 
 		if (clockTstruct.tm_year > 100)
 		{
@@ -51,6 +63,8 @@ void ClockComponent::update(int deltaTime)
 				clockBuf = Utils::Time::timeToString(clockNow, "%I:%M %p");
 			else
 				clockBuf = Utils::Time::timeToString(clockNow, "%H:%M");
+
+			LOG(LogInfo) << "[ThemeDebug] ClockComponent::update tag=" << getTag() << " setText(\"" << clockBuf << "\")";
 
 			setText(clockBuf);
 		}
